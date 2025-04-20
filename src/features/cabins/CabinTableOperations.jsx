ba85f66af380fa0adb96ea2b328793ccf66db248
arafatah <arafathowlader890@gmail.com>
2025-04-20 11:34:36 +0600

    feat: enhance filtering functionality in CabinTable and Filter components

--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -2,9 +2,15 @@ import Filter from "../../ui/Filter";
 import TableOperations from "../../ui/TableOperations";
 
 function CabinTableOperations() {
+  const discountOptions = [
+    { value: "all", label: "All" },
+    { value: "no-discount", label: "No Discount" },
+    { value: "with-discount", label: "With Discount" },
+  ];
+
   return (
     <TableOperations>
-      <Filter />
+      <Filter filterField="discount" options={discountOptions} />
     </TableOperations>
   );
 }
