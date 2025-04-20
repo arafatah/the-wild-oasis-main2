b67f95b53f02174a52e14293ed514257d1ee38e5
arafatah <arafathowlader890@gmail.com>
2025-04-20 12:24:13 +0600

    feat: implement sorting functionality in CabinTable and fix onChange handler in SortBy component

--- src/ui/Select.jsx
+++ src/ui/Select.jsx
@@ -16,7 +16,7 @@ const StyledSelect = styled.select`
 
 function Select({ options, value, onChange, ...props }) {
   return (
-    <StyledSelect value={value} onChange={onchange} {...props}>
+    <StyledSelect value={value} onChange={onChange} {...props}>
       {options.map((option) => (
         <option value={option.value} key={option.value}>
           {option.label}
--- src/ui/SortBy.jsx
+++ src/ui/SortBy.jsx
@@ -1,9 +1,23 @@
+import { useSearchParams } from "react-router-dom";
 import Select from "./Select";
 
 function SortBy({ options }) {
-  function handleChange() {}
+  const [searchParams, setSearchParams] = useSearchParams();
+  const sortBy = searchParams.get("sortBy") || "";
 
-  return <Select options={options} type="white" onChang={handleChange} />;
+  function handleChange(e) {
+    searchParams.set("sortBy", e.target.value);
+    setSearchParams(searchParams);
+  }
+
+  return (
+    <Select
+      options={options}
+      type="white"
+      value={sortBy}
+      onChange={handleChange}
+    />
+  );
 }
 
 export default SortBy;
