065ec738980a108e6bd29691559cf9807cff4597
arafatah <arafathowlader890@gmail.com>
2025-04-24 11:46:50 +0600

    feat: enhance login functionality to reset email and password fields on submission

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -15,9 +15,17 @@ function LoginForm() {
   function handleSubmit(e) {
     e.preventDefault();
     if (!email || !password) return;
-    login({ email, password });
-  }   
-   
+    login(
+      { email, password },
+      {
+        onSettled: () => {
+          setEmail("");
+          setPassword("");
+        },
+      }
+    );
+  }
+
   return (
     <Form onSubmit={handleSubmit}>
       <FormRow label="Email address">
