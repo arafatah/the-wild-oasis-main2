cccdf3de202f6e3925b85f270fb5b1a762292e9d
arafatah <arafathowlader890@gmail.com>
2025-04-24 11:42:15 +0600

    feat: add isAuthenticated property to useUser hook and update ProtectedRoute for authentication handling

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,12 +1,41 @@
-function ProtectedRoute(children) {
-  // 1. Load the authenticated user 
+import styled from "styled-components";
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-  // 2. While loading, show a spinner 
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color--gray-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
-  // 3. If there is NO authenticated user, redirect to the /login 
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
 
-  // 4. If there is user, render the app 
-  return children;
+  // 1. Load the authenticated user
+  const { isPending, isAuthenticated } = useUser();
+
+  // 3. If there is NO authenticated user, redirect to the /login
+  useEffect(
+    function () {
+      if (!isAuthenticated && !isPending) navigate("/login");
+    },
+    [isAuthenticated, navigate, isPending]
+  );
+
+  // 3. While loading, show a spinner
+  if (isPending)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
+  // 4. If there is user, render the app
+  if (isAuthenticated) return children;
 }
 
 export default ProtectedRoute;
