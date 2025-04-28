62cf7b1c268bba2b3ebe3fd0c07627c0028cd60f
arafatah <arafathowlader890@gmail.com>
2025-04-28 12:34:38 +0600

    feat: enhance SalesChart component with dark mode support and dynamic data handling

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -35,7 +35,7 @@ function DashboardLayout() {
       />
       <div>Today's activity</div>
       <div>Chart stay duration</div>
-      <SalesChart />
+      <SalesChart bookings={bookings} numDays={numDays}/>
     </StyledDashboardLayout>
   );
 }
